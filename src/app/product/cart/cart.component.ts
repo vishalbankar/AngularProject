4652fc451060d64bee8b84ec791585a0ef323609
vishalbankar <vishalbankar251@gmail.com>
2020-09-19 19:07:41 +0530

    added Addition logic

--- src/app/product/cart/cart.component.ts
+++ src/app/product/cart/cart.component.ts
@@ -8,31 +8,46 @@ import {ProductService} from '../services/product.service';
 })
 export class CartComponent implements OnInit {
 
+  public totalAmount;
   public cartPrdArr:any[];
   public prodArray:any = [];
   public cartProdList:any = [];
   constructor(private prdCart:ProductService) { }
 
   ngOnInit() {
-    this.prdCart.getProdData();
-    if(this.prdCart.productArr.length >0){
-      this.prodArray = this.prdCart.productArr;
-      this.cartPrdArr=this.prdCart.cartProdArr;
+    this.prdCart.getProdData().subscribe(data => {this.prodArray = data;
+    console.log(data);
+    if(data.length>0){
+      this.loadCartData(data);
+    }  
+      });
+
+  }
+
+  loadCartData(prodArr){
+    this.prodArray = prodArr;
+    console.log(this.prodArray);
     
+    if(this.prodArray.length >0){
+    this.cartPrdArr=this.prdCart.cartProdArr;
      for(let i=0; i < this.prodArray.length;i++){
       for(let j=0; j < this.cartPrdArr.length; j++){
           if(this.prodArray[i].ProdId == this.cartPrdArr[j]){
             this.cartProdList.push(this.prodArray[i]);
+            this.totalAmountCal();
           } 
       } 
      }
-    console.log(this.cartProdList);  
     }
-      
+
   }
-
-  
-
-
-
+  totalAmountCal(){
+        // this.totalAmount=this.cartProdList.ProductPrice
+        this.totalAmount = this.cartProdList.reduce(function(tot=0, arr) { 
+        // return the sum with previous value
+        console.log(tot + parseInt(arr.ProdPrice));
+        return tot = tot + parseInt(arr.ProdPrice);
+        // set initial value as 0
+      },0);
+  }
 }
